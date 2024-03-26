e3929fad804540822479d54d3e5e534052e7af54
alan <kno12350@gmail.com>
2024-03-26 15:01:25 +0900

    SharedPreferences 라이브러리로 기기에 파일 저장하기 1

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,19 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+void main() async {
+  // main() 함수에서 async를 쓰려면 필요
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // shared_preferences 인스턴스 생성
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+
   runApp(
     MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (context) => CatService()),
+        ChangeNotifierProvider(create: (context) => CatService(prefs)),
       ],
       child: const MyApp(),
     ),
@@ -35,7 +42,10 @@ class CatService extends ChangeNotifier {
   //좋아요 사진
   List<String> favoriteImages = [];
 
-  CatService() {
+  // SharedPreferences 인스턴스
+  SharedPreferences prefs;
+
+  CatService(this.prefs) {
     getRandomCatImages();
   }
 
